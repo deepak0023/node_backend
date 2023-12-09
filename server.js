52179bb0768dcbc3492c6cca68bfd38ef90cfacb
deepak <deepamdeebu@gmail.com>
2023-12-09 21:44:52 +0530

    Routes : Add routes required routes for getting json response

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 require('dotenv').config()
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
+
+// middleware
+app.use(express.json())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
+
+// routes
+app.use('/api/workouts', workoutRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
